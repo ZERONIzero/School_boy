bafc21a71b432fe8288c7d2e01ecb7050743c53e
ZeroNI <Brandodio9tt@gmail.com>
2025-06-25 00:15:03 +0300

    demo 0.0.1

---
+++ tool/mail.js
@@ -0,0 +1,10 @@
+module.exports = {
+    pool: true,
+    host: 'smtp.yandex.ru',
+    port: 465,
+    secure: true,
+    auth: {
+        user: "ERRORZERONI@yandex.ru",
+        pass: "ounukprovfassyvq",
+    }
+};
